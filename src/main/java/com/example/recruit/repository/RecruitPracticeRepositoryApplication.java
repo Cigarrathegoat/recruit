8e57eb2dbcb63c49f3d2a03a567e7e24b8683396
Silvio Maia <ingles.silvio@gmail.com>
2023-09-04 14:52:44 -0300

    first commit

---
+++ src/main/java/com/example/recruit/repository/RecruitPracticeRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.recruit.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitPracticeRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitPracticeRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/recruit/repository/RecruitPracticeRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recruit.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitPracticeRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
